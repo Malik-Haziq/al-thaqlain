985b073348b5864c54465e0c65efa3f723a37c4b
Malik-Haziq <malikhaziq153@gmail.com>
2024-05-12 17:41:03 +0500

    added navigation

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+export function Button(_props: { Text: string }) {
+  return (
+    <>
+      <h1 className="text-blue-500 text-3xl">{_props.Text}</h1>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: { 400: "#ffd315", 500: "#ffd316" },
+      },
+    },
   },
   plugins: [],
 };
