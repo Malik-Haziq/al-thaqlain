bd037db851ebba703160bd2fb072eb6f9d1df8bb
Malik-Haziq <malikhaziq153@gmail.com>
2024-05-13 17:57:50 +0500

    completed header

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,22 @@
+import { Button } from "../../components/Button";
+
 export function Home() {
   return (
     <>
-      <h1 className="text-blue-500 text-3xl">Home</h1>
+      <header className="bg-headerBg h-[80vh] bg-no-repeat bg-cover flex justify-center">
+        <div className="max-w-screen-xl p-4 mx-auto flex flex-col gap-4 justify-center">
+          <h1 className="text-5xl sm:text-7xl xl:text-8xl font-medium text-white font-playfairDisplay">
+            Helping you visit&nbsp;
+            <span className="text-secondary-400">Islamic regions</span>
+          </h1>
+          <p className="text-white text-lg sm:text-xl md:text-2xl sm:w-2/3 mb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, nulla
+            eos! Eum natus odio vitae libero repellat, reprehenderit quas
+            numquam.
+          </p>
+          <Button text="Get in touch" />
+        </div>
+      </header>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        secondary: { 400: "#ffd315", 500: "#ffd316" },
+        secondary: {
+          100: "#FFE985",
+          400: "#F5C800",
+          500: "#E0B700",
+          600: "#CCA700",
+        },
+      },
+      backgroundImage: {
+        headerBg:
+          "linear-gradient(rgba(1,1,1,.5), rgba(1,1,1,.9)), url('/src/assets/home/Arbaeen.jpg.webp')",
       },
     },
+    fontFamily: { playfairDisplay: "Playfair Display" },
   },
   plugins: [],
 };
