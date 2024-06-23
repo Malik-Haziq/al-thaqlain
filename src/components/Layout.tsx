95d2ae94fb6d7416fd10208bd01256420cabeca4
Malik-Haziq <malikhaziq153@gmail.com>
2024-06-23 18:35:21 +0500

    chnages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,10 @@
 import { Navbar, Footer } from "./navigation";
 import { Outlet } from "react-router-dom";
-import { ScrollToTop } from "./ScrollToTop";
 
 export function Layout() {
   return (
     <>
       <main className="bg-black-400 text-white-500">
-        {/* <ScrollToTop /> */}
         <Navbar />
         <Outlet />
         <Footer />
