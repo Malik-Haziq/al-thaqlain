df56cfaf172722073238d01a0561e1fbc0a9795e
devsyedhaider <devalihaider86@gmail.com>
2024-11-13 20:51:35 +0500

    change header text color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header(_props: { heading: string; img: string }) {
         style={{ backgroundImage: `url(${_props.img})` }}
         className="mb-32 h-[30vh] md:h-[40vh] bg-no-repeat bg-center bg-cover text-center flex items-center justify-center"
       >
-        <h1 className="text-4xl md:text-7xl text-white-100">
+        <h1 className="text-4xl md:text-7xl text-black-100">
           {_props.heading}
         </h1>
       </div>
